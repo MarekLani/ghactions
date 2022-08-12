59125a8b642d4d060e1e60c00fe55dcc3296804d
mlani <mareklani@gmail.com>
2022-08-12 10:41:03 +0200

    added main.tf

---
+++ tf/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.11.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
